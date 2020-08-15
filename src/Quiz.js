b732497f88676d7a7ac2776138ae8cb4cf39b27a
psk <ps.karthik9@gmail.com>
2020-08-15 16:15:15 +0530

    quiz-1

---
+++ src/Quiz.js
@@ -0,0 +1,55 @@
+import React,{Component} from 'react';
+import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
+
+let quizData = require('./quiz_data.json');
+
+
+class Quiz extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            quiz_position:1
+        }
+    }
+    showNextQuestion(){
+        this.setState({
+            quiz_position:this.state.quiz_position+1
+            }
+        );
+    }
+
+    handleResetClick(){
+        this.setState({
+            quiz_position:1
+        });
+    }
+   
+
+    render(){
+      
+
+        const isQuizEnd=this.state.quiz_position-1===quizData.quiz_questions.length;
+        
+   
+
+        if(isQuizEnd){
+            return(
+                <div>
+                    <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
+                </div>
+            );
+        }
+        else{
+           
+            return(
+                <div>
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                    showNextQuestionHandler={this.showNextQuestion.bind(this)} />
+                </div>
+            );
+        }
+    }
+}
+export default Quiz;
